e85a16f6849781be99cd8b6235e0d45cbdcdde2c
gaurav <kotharigaurav6@gmail.com>
2024-12-31 16:04:03 +0530

    env mongo_uri package_json

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,8 @@
 // const url = "mongodb://127.0.0.1:27017/facultyrecruitmentsystem"
 // export default url;
 
-const url = "mongodb+srv://kotharigaurav6:gaurav_kothari1992@cluster0.hpwg6.mongodb.net/merndb?retryWrites=true&w=majority&appName=Cluster0";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const url = process.env.MONGO_URI;
 export default url;
